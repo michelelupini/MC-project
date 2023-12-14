625ab9f397b0160e5f2030728d1245c2cafc8865
stefanoviel <stefi.viel@gmail.com>
2023-12-14 16:40:00 +0100

    changes

--- run_experiments.py
+++ run_experiments.py
@@ -118,9 +118,7 @@ def run_multiple_experiments(repeat_n_times, beta, m, d, s, fixed_ones=False, si
         list_errors.append(errors[-1])
         steps_to_converge.append(len(errors))
 
-    return beta, np.mean(list_errors), np.mean(steps_to_converge)
-
-
+    return beta, list_errors, np.mean(steps_to_converge)
 
 
 def run_multiple_multi_proc(repeat_n_times, betas, m, d, s, fixed_ones=False, sign=False, **args): 
@@ -144,47 +142,3 @@ def run_multiple_multi_proc(repeat_n_times, betas, m, d, s, fixed_ones=False, si
             print('beta=', beta, 'n_iterations=', n_iterations, 'loss', loss)
 
     print('Best result: beta=', best_beta, 'n_iterations=', best_iterations, 'loss', best_loss)
-
-
-
-def run_multiple_multi_proc_sim_annealing(repeat_n_times, betas, m, d, s, mul_increases, increase_everys, fixed_ones=False, sign=False, iterations = 0): 
-
-
-    best_beta = 0
-    best_loss = math.inf
-    best_iterations = math.inf
-
-    with ProcessPoolExecutor() as executor:
-        futures = []
-        for beta in betas: 
-            for mul_inc in mul_increases: 
-                for increase_every in increase_everys: 
-                    futures.append(executor.submit(run_multiple_experiments, repeat_n_times, beta, m, d, s, fixed_ones=fixed_ones, 
-                                                   sign=sign, change_beta_every= increase_every, update_beta=mul_inc, iterations=iterations))
-
-        for future in concurrent.futures.as_completed(futures):
-            beta, loss, n_iterations = future.result()
-
-            if loss <= best_loss and n_iterations <= best_iterations:
-                best_beta = beta
-                best_loss = loss
-                best_iterations = n_iterations
-
-            print('beta=', beta, 'n_iterations=', n_iterations, 'loss', loss)
-
-    print('Best result: beta=', best_beta, 'n_iterations=', best_iterations, 'loss', best_loss)
-
-
-
-
-
-
-def run_multiple_ms(repeat_n_times, beta, m_values, d, s, mul_increase, increase_every, fixed_ones=False, sign=False, iterations = 0):
-    with ProcessPoolExecutor() as executor:
-        futures = []
-        for m_value in m_values: 
-            futures.append(executor.submit(repeat_n_times, beta, m_value, d, s, mul_increase, increase_every, fixed_ones=fixed_ones, sign=sign, iterations = iterations))
-
-        losses_with_different_m = [result[0] for result in futures]
-        
-    return losses_with_different_m
